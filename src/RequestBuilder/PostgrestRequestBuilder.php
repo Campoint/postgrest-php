0401c1794f0d9b448809c9c31291a167cdb1f4fa
Kirill Fuks <kf@campoint.net>
2023-07-24 11:44:21 +0200

    fix: code style, remove unused import

--- src/RequestBuilder/PostgrestRequestBuilder.php
+++ src/RequestBuilder/PostgrestRequestBuilder.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace PostgrestPhp\RequestBuilder;
 
-use Exception;
 use PostgrestPhp\RequestBuilder\Enums\CountType;
 use PostgrestPhp\RequestBuilder\Enums\DataFormat;
 use PostgrestPhp\RequestBuilder\Enums\DuplicateResolution;
